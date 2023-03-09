2a353ad6d1a18a68b4c45f7ec6c83159f8adc9c5
jungha Baek <junghabaek@gmail.com>
2023-03-09 14:12:10 +0900

    Hello World

--- src/main/java/com/jungha/ShoppingMall/ShoppingMallApplication.java
+++ src/main/java/com/jungha/ShoppingMall/ShoppingMallApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingMallApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ShoppingMallApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
